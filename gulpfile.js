b6c6457da12bd29935c668f458eeaa0d031bd3f8
edwardzhong <edwardloveyou@gmail.com>
2019-02-22 19:05:05 +0800

    add scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,48 @@
-var gulp = require('gulp');
-var del = require('del');
-var less = require('gulp-less');
-var concatCss = require('gulp-concat-css');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var watch = require('gulp-watch');
-var name ='base';//生成文件名称
+const { src, dest, watch, series } = require('gulp');
+const del = require('del');
+const less = require('gulp-less');
+const concatCss = require('gulp-concat-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const name = 'base';//生成文件名称
+sass.compiler = require('node-sass');
 
-gulp.task('watch', function () {
-    return watch(['./less/**/*.less'], function (){
-        gulp.src('./less/**/*.less')
+
+function clean(cb) {
+    return del(['./dist', './css'], cb);
+}
+
+function parseLess() {
+    return src('./less/index.less')
         .pipe(less())
-        .pipe(gulp.dest('./css'));
-    });
-});
+        .pipe(dest('./css'));
+}
 
-gulp.task('clean', function(cb) {
-    return del(['./dist','./css'], cb);
-});
+function parseScss(){
+    return src('./scss/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
+}
 
-gulp.task('less', function () {
-    return  gulp.src('./less/index.less')
-        .pipe(less())
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('css',['less'],function() {
-    return gulp.src(['css/index.css'])
-        .pipe(postcss([ autoprefixer({
-                "browsers": ["last 2 version", "> 0.5%", "ie 6-8","Firefox < 20"]
+function css() {
+    return src(['css/index.css'])
+        .pipe(postcss([
+            autoprefixer({
+                "browsers": ["last 2 version", "> 0.5%", "ie 6-8", "Firefox < 20"]
                 // "browsers": ["last 2 version", "> 0.1%"]
             })
         ]))
-        .pipe(rename(name+'.css'))
-        .pipe(gulp.dest('./dist'))
+        .pipe(rename(name + '.css'))
+        .pipe(dest('./dist'))
         .pipe(cleanCSS())//代替 gulp-minify-css
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task("default", ['css']);
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./dist'));
+}
+// series 从左至右依次串行执行任务
+// parallel 并行执行
+// watch('./less/**/*.less', series(parseLess, css));
+watch('./scss/**/*.scss', series(parseScss, css));
+exports.default = series(clean, parseScss, css);
