68aa2b8add468c7144549c6bb6dbd79ce559e841
edwardzhong <edwardloveyou@gmail.com>
2019-07-02 15:01:07 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 const { src, dest, watch, series } = require('gulp');
 const del = require('del');
-const concatCss = require('gulp-concat-css');//合并css
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const less = require('gulp-less');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const name = 'base';//生成文件名称
@@ -14,34 +11,18 @@ function clean(cb) {
     return del(['./dist', './css'], cb);
 }
 
-function parseLess() {
-    return src('./less/index.less')
-        .pipe(less())
-        .pipe(dest('./css'));
-}
-
-function parseScss() {
+function scss() {
     return src('./scss/index.scss')
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))//outputStyle: expanded,compact,compressed
-        .pipe(dest('./css'));
-}
-
-function css() {
-    return src(['css/index.css'])
-        .pipe(postcss(
-            // [
-            // autoprefixer({
-            //     browsers: ["last 2 version", "> 5%", "Firefox >= 20", 'ie >= 8', 'iOS >= 6', 'Android >= 4']//["last 2 version", "> .5%","ie 6-8","not ie <= 8"]
-            // })]
-        ))
+        .pipe(postcss())
         .pipe(rename(name + '.css'))
-        .pipe(dest('./dist'))
+        .pipe(dest('css'))//输出未压缩的css
         .pipe(cleanCSS({ compatibility: 'ie8' }))//压缩代码，兼容浏览器，优化代码
         .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('./dist'));
+        .pipe(dest('dist'));
 }
+
 // series   从左至右依次串行执行任务
 // parallel 并行执行任务
-// watch('./less/**/*.less', series(parseLess, css));
-watch('./scss/**/*.scss', series(parseScss, css));
-exports.default = series(clean, parseScss, css);
+watch('./scss/**/*.scss', scss);
+exports.default = series(clean, scss);
