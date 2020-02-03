0725ea261ce622eb85b1dc7720f9e70639af3af6
edwardzhong <zhongjfgd@163.com>
2020-02-04 01:26:52 +0800

    tab

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ const postcss = require('gulp-postcss');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
-const name = 'base';//生成文件名称
+const name = 'base'; //生成文件名称
 sass.compiler = require('node-sass');
 
 function clean(cb) {
-    return del(['./dist', './css'], cb);
+  return del(['./dist', './css'], cb);
 }
 
 function scss() {
-    return src('./scss/index.scss')
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))//outputStyle: expanded,compact,compressed
-        .pipe(postcss())
-        .pipe(rename(name + '.css'))
-        .pipe(dest('css'))//输出未压缩的css
-        .pipe(cleanCSS({ compatibility: 'ie8' }))//压缩代码，兼容浏览器，优化代码
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('dist'));
+  return src('./scss/index.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError)) //outputStyle: expanded,compact,compressed
+    .pipe(postcss())
+    .pipe(rename(name + '.css'))
+    .pipe(dest('css')) //输出未压缩的css
+    .pipe(cleanCSS({ compatibility: 'ie8' })) //压缩代码，兼容浏览器，优化代码
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('dist'));
 }
 
 // series   从左至右依次串行执行任务
